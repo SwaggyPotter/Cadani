0cf579fbe2a3c081b533c991d21fd85709eccf3f
Tim <89969636+SwaggyPotter@users.noreply.github.com>
2026-05-15 14:43:47 +0200

    Update

---
+++ cadani-solar/src/data/funding.ts
@@ -0,0 +1,18 @@
+export interface FundingDocument {
+  label: string;
+  href: string;
+  ariaLabel: string;
+}
+
+export const fundingDocuments: FundingDocument[] = [
+  {
+    label: 'Förderrichtlinie',
+    href: 'https://cdn1.site-media.eu/images/document/23380372/foerderrichtlinie-solarplus-2026-2-1DYbql6LTu05BS2_YAUA_g.pdf',
+    ariaLabel: 'SolarPLUS Förderrichtlinie als PDF in neuem Tab öffnen',
+  },
+  {
+    label: 'Allgemeine Infos',
+    href: 'https://cdn1.site-media.eu/images/document/7857779/2.pdf',
+    ariaLabel: 'Allgemeine SolarPLUS Informationen als PDF in neuem Tab öffnen',
+  },
+];
