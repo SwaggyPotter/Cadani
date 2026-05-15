9164b40017a213d3cf241dcf4c54ac0296314968
Tim <89969636+SwaggyPotter@users.noreply.github.com>
2026-05-15 14:30:17 +0200

    Hero Banner

---
+++ cadani-solar/src/data/partners.ts
@@ -0,0 +1,33 @@
+export interface Partner {
+  name: string;
+  href: string;
+  logo: string;
+  alt: string;
+}
+
+export const partners: Partner[] = [
+  {
+    name: 'WRT',
+    href: '#',
+    logo: 'https://cdn1.site-media.eu/images/0/18054066/WRT_Linie_RGB_pos-EXlHqe3yTqGYHxoUejfeIQ.jpg',
+    alt: 'WRT Partnerlogo',
+  },
+  {
+    name: 'IBC Solar',
+    href: '#',
+    logo: 'https://cdn1.site-media.eu/images/0/18053988/IBC_SOLAR-Logo-RGB-j5alRjHe5GqILAa7mX1yXg.svg',
+    alt: 'IBC Solar Partnerlogo',
+  },
+  {
+    name: 'Garten24h',
+    href: 'https://www.garten24h.de/',
+    logo: 'https://cdn1.site-media.eu/images/0/15001542/Element1.svg',
+    alt: 'Garten24h Partnerlogo',
+  },
+  {
+    name: 'SR Bauunternehmen',
+    href: 'https://www.srbauunternehmen.de/',
+    logo: 'https://cdn1.site-media.eu/images/0/15001565/Logo_SR_rotee.svg',
+    alt: 'SR Bauunternehmen Partnerlogo',
+  },
+];
